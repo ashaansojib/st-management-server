2af0280b4c4e62bc9c1bc617acd19fe75d2e91c2
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-09-17 03:33:27 +0600

    add 2 route

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 9988
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 
 const uri = "mongodb+srv://ashaduzzamansojib67:ZjMlKPajdMvFp31l@cluster0.ugrpd0k.mongodb.net/?retryWrites=true&w=majority";
@@ -24,16 +24,21 @@ async function run() {
     try {
         const stManageDB = client.db('sujon-telecom').collection('PackagesList');
 
-        app.get('/packages', async(req, res)=>{
+        app.get('/packages', async (req, res) => {
             const result = await stManageDB.find().toArray();
             res.send(result)
         });
-        app.post('/packages', async(req, res) =>{
+        app.post('/packages', async (req, res) => {
             const query = req.body;
-            console.log(query)
             const stored = await stManageDB.insertOne(query);
             res.send(stored)
         });
+        app.delete('/delete-combo/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await stManageDB.deleteOne(query)
+            res.send(result)
+        })
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         // Send a ping to confirm a successful connection
